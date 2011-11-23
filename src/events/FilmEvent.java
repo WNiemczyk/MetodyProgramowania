f9abad50479ec0f46441eea9f686b0e2ac0e2f6d
5Nm-+9Dc <wojniemczyk@gmail.com>
2011-11-23 16:24:01 +0100

    final version

--- src/events/FilmEvent.java
+++ src/events/FilmEvent.java
@@ -8,16 +8,17 @@ import films.Shelf;
 
 public class FilmEvent extends EventObject {
 
-	private static final long serialVersionUID = 1L;
+private static final long serialVersionUID = 1L;
 
-	private Film _film;
+private Film _film;
 
-	public FilmEvent(Object source, Film film) {
-		super(source);
-		_film = film;
-	}
+public FilmEvent(Object source, Film film) {
+super(source);
+_film = film;
+}
 
-	public Film getFilm() {
-		return _film;
-	}
+public Film getFilm() {
+return _film;
+}
+
 }
--- src/events/FilmListener.java
+++ src/events/FilmListener.java
@@ -1,10 +1,10 @@
 package events;
 
 public interface FilmListener {
-	
-	public void filmBorrowed(FilmEvent event);
-	public void filmReturned(FilmEvent event);
-	public void filmReserved(FilmEvent event);
-	public void filmAdded(FilmEvent event);
+
+public void filmBorrowed(FilmEvent event);
+public void filmReturned(FilmEvent event);
+public void filmReserved(FilmEvent event);
+public void filmAdded(FilmEvent event);
 
 }
